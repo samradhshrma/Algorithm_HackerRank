2e8182b5c3dd97fb172e4cbb28c81f3e61ae079b
samradhshrma <51164105+samradhshrma@users.noreply.github.com>
2021-06-25 12:17:12 +0530

    Create Staircase.cs

---
+++ Staircase.cs
@@ -0,0 +1,38 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            var space_num = new String(' ', n - i);
+            var hashe_num = new String('#', i);
+            System.Console.WriteLine(space_num + hashe_num);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
