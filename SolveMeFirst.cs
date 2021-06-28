9a400b515c05d5133ea44fd11d82ab3aaee4d21a
samradhshrma <51164105+samradhshrma@users.noreply.github.com>
2021-06-28 11:53:28 +0530

    Update SolveMeFirst.cs

--- SolveMeFirst.cs
+++ SolveMeFirst.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-class Solution {
+class Solution 
+{
 
-    static int solveMeFirst(int a, int b) {
-      return a+b; 
-     }
+    static int solveMeFirst(int a, int b) 
+    {
+        return a+b; 
+    }
 
-    static void Main(String[] args) {
+    static void Main(String[] args) 
+    {
         int val1 = Convert.ToInt32(Console.ReadLine());
         int val2 = Convert.ToInt32(Console.ReadLine());
         int sum = solveMeFirst(val1,val2);
