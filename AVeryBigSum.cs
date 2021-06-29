f830ab49e9089ab23d70f59212bb7a8997d61338
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-06-29 16:58:29 +0530

    Update AVeryBigSum.cs

--- AVeryBigSum.cs
+++ AVeryBigSum.cs
@@ -16,7 +16,7 @@ class Result
 {
     public static long aVeryBigSum(List<long> ar)
     {
-        long sum=0;
+        var sum = 0L;
         for(int i=0;i< ar.Count;i++)
         {
             sum+=ar[i];
