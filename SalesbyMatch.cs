db6347c9688ee61fe989d2a066da8852af91a780
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-06-30 14:15:18 +0530

    Create SalesbyMatch.cs

---
+++ SalesbyMatch.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*link: https://www.hackerrank.com/challenges/sock-merchant/problem?h_r=next-challenge&h_v=zen
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+         var pair=0;
+         var max=ar.Max()+1;
+         for(var i=0;i<n;i++)
+         {
+            if(ar[i]==max)
+                continue;
+            for(var j=i+1;j<n;j++)
+            {
+                if(ar[i]==ar[j])
+                {
+                    ar[i]=max;
+                    ar[j]=max;
+                    pair++;
+                    break;
+                }
+            }
+         }
+         return pair;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
