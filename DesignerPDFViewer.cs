dd81848c568b913862c7c249f10f6cf71e8ff58e
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-07-01 15:26:15 +0530

    Create DesignerPDFViewer.cs

---
+++ DesignerPDFViewer.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*link : https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        return word.ToCharArray().Max(ch => h[ch - 'a']) * word.Length;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
