2bd503e3d80ac9f4d9e893caee875ae4fd488876
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-07-02 12:16:03 +0530

    Create UtopianTree.cs

---
+++ UtopianTree.cs
@@ -0,0 +1,57 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*link: https://www.hackerrank.com/challenges/utopian-tree/problem
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+    
+    public static int utopianTree(int n)
+    { 
+      var hieght=1;
+      for(var i=0;i<n;i++)
+      {
+       hieght = i%2==0 ? hieght*=2 : hieght+=1;
+      }
+      return hieght;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.utopianTree(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
