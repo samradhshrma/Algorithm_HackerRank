0666489c7c71fd868f1931d66bc6d1b71e32ba56
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-07-05 13:16:23 +0530

    Create SavethePrisoner.cs

---
+++ SavethePrisoner.cs
@@ -0,0 +1,74 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*link: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+     * Complete the 'saveThePrisoner' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     *  3. INTEGER s
+     */
+
+    public static int saveThePrisoner(int n, int m, int s)
+    {
+        var prisoners = n - s + 1;
+        if (m > prisoners)
+        {
+            s = 1;
+            m -= prisoners;
+            m = m % n;
+        }
+
+        if (m == 0)
+            s = n;
+        else
+            s += m - 1;
+
+        return s;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int s = Convert.ToInt32(firstMultipleInput[2]);
+
+            int result = Result.saveThePrisoner(n, m, s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
