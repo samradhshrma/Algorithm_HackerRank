f07826397f5d6125889f648fa299388b442d2c13
SAMRADH SHARMA <51164105+samradhshrma@users.noreply.github.com>
2021-07-05 12:13:56 +0530

    Create ViralAdvertising.cs

---
+++ ViralAdvertising.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*link: https://www.hackerrank.com/challenges/strange-advertising/problem
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n)
+    {
+        var totalLikes = 0D;
+        var shares = 5.0;
+        for (var i = 0; i < n; i++)
+        {
+            totalLikes += Math.Floor(shares / 2);
+            shares = Math.Floor(shares / 2) * 3;
+        }
+        int sum = (int)totalLikes;
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.viralAdvertising(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
